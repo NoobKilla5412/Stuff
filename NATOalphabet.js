d66a6110a9b506ff3bf59a0e7527307bba92b4fd
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2024-01-09 14:06:55 -0800

    update

--- NATOalphabet.js
+++ NATOalphabet.js
@@ -1,6 +1,10 @@
 // @ts-check
 
 define(async (req, exports, module, args) => {
+  const { openGUI, prompt } = await req("./js/GUI");
+  const { Button, ButtonRef } = await req("./js/Button");
+  const { createElement } = await req("./js/HTMLUtils");
+
   const alphabet = {
     a: "alpha",
     b: "bravo",
@@ -30,11 +34,18 @@ define(async (req, exports, module, args) => {
     z: "zulu"
   };
 
-  const text = "";
-
-  writeLn(
-    text.replace(/\w/g, (x) => {
+  function convert(text) {
+    return text.replace(/\w/g, (x) => {
       return (alphabet[x.toLowerCase()] || x) + " ";
-    })
-  );
+    });
+  }
+
+  Button("button", "Enter Text", {
+    async onclick() {
+      const text = await prompt("Enter the text");
+      const gui = await openGUI();
+      gui.addElement(createElement("div", convert(text)));
+      gui.addCancel("Close");
+    }
+  });
 });
